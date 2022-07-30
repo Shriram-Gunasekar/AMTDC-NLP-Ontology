6a2381d5092cc0c6980c6fcd77aba5f487abe1cb
shriramg <gshriram14@gmail.com>
2022-07-30 10:57:59 +0530

    New MEssae

---
+++ data.py
@@ -0,0 +1,13 @@
+# NLP Data Classes
+
+class Material:
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.spec = spec
+    
+    def __len__(self):
+        pass
+    
+    def __getitem__(self, key):
+        pass
+
