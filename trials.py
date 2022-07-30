b4ea342e310de39dadd69a8f277b2fe81aeee0c1
shriramg <gshriram14@gmail.com>
2022-07-30 12:21:05 +0530

    Similarity checking

--- trials.py
+++ trials.py
@@ -6,3 +6,6 @@ import en_core_web_md
 nlp = spacy.load('en_core_web_md')
 doc = nlp('Give me a tool with steel material and ')
 
+sent1 = nlp('material of grade')
+sent2 = nlp('tool of grade')
+print(sent1.similarity(sent2))
