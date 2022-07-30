e993e2e8626345f45548d68d0ec7c9be2d1c797d
shriramg <gshriram14@gmail.com>
2022-07-30 13:27:55 +0530

    POS May Work

--- trials.py
+++ trials.py
@@ -1,23 +1,11 @@
-#trials
 import re
 import spacy
 import en_core_web_md
 from data import Holder, Insert, getquery
-#Initialized Model
 nlp = spacy.load('en_core_web_md')
-doc = nlp(getquery())
-
-#class Similarity:
-#    material = ["of material", "having material","having material of", "of material of", "of material having"]
-#    coating = ["of coating", "having coating", "having coating of", "of coating of", "of coating having"]
-#    catalog = ["catalog number KM50TSMDJNR1506"," catalog number DNMG150604MS","DNMG150604MS","KM50TSMDJNR1506"]
-#    name = ["M-Clamping","Boring bar","Welding Bar","Threading Holder"]
-#
-#catalog = ["catalog number KM50TSMDJNR1506"," catalog number DNMG150604MS","DNMG150604MS","KM50TSMDJNR1506"]
-#for i in range(len(catalog)):
-#    for j in range(len(catalog)):
-#        if i != j:
-#            print(catalog[i], catalog[j])
-#            print(nlp(catalog[i]).similarity(nlp(catalog[j])))
 
 
+doc = nlp("We need an M-Clamping material of catalog number KM50TSMDJNR1506 catalog number KM50TSMDJNR1506 P-Steel") 
+L = []
+for token in doc:
+    print(token.text,token.pos_)
