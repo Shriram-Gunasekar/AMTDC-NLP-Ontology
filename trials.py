bb3ce2aa4dcdb3e7837071c1518c8c2b0898dc32
shriramg <gshriram14@gmail.com>
2022-07-30 13:51:44 +0530

    Tuples

--- trials.py
+++ trials.py
@@ -1,11 +1,17 @@
 import re
 import spacy
 import en_core_web_md
-from data import Holder, Insert
+from data import *
 nlp = spacy.load('en_core_web_md')
 
-
-doc = nlp("We need an M-Clamping material of catalog number KM50TSMDJNR1506 catalog number KM50TSMDJNR1506 P-Steel") 
+doc = nlp("We need an M-Clamping material of catalog number" 
+          "KM50TSMDJNR1506 catalog number KM50TSMDJNR1506 P-Steel") 
 L = []
 for token in doc:
-    print(token.text,token.pos_)
+    L.append((token.text,token.pos_))
+for tup in L:
+    if tup[0] == '-':
+        L.pop(L.index(tup)-1)
+        L.remove(L.index(tup)+1)
+        L.remove(tup)
+print(L)
