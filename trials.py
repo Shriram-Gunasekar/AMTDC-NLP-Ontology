52d9f36280b35c525f7c1ed599e83b9674f5734b
shriramg <gshriram14@gmail.com>
2022-07-30 18:35:47 +0530

    Extracting Catalogue Number

--- trials.py
+++ trials.py
@@ -1,22 +1,32 @@
-import re
+#import re
 from difflib import SequenceMatcher
-import spacy
-import en_core_web_md
-from data import *
-nlp = spacy.load('en_core_web_md')
+#import spacy
+#import en_core_web_md
+#from data import *
+#nlp = spacy.load('en_core_web_md')
 
-doc = nlp("We need an M-Clamping material of catalog number" 
-          "KM50TSMDJNR1506 catalog number KM50TSMDJNR1506 P-Steel") 
-L = []
-for token in doc:
-    L.append((token.text,token.pos_))
-for tup in L:
-    if tup[0] == '-':
-        L.pop(L.index(tup)-1)
-        L.remove(L[L.index(tup)+1])
-        L.remove(tup)
-print(L)
+#def query():
+#    query = input("Enter your query: ")
+#    return query
+#
+#query = query()
+#query = query.lower()
+#doc = nlp(query) 
+
+#L = []
+#for token in doc:
+#    L.append((token.text,token.pos_))
+#for tup in L:
+#    if tup[0] == '-':
+#        L.pop(L.index(tup)-1)
+#        L.remove(L[L.index(tup)+1])
+#        L.remove(tup)
+#print(L)
 
 def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
+    return SequenceMatcher(None, a.lower(), b.lower()).ratio()
+
+pattern = "[a-zA-Z]*[1-9]*"
+
+
     
