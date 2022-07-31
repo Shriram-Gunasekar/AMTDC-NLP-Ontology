3a4607e8add1b07ba0172b947ca375bfebc0ab45
shriramg <gshriram14@gmail.com>
2022-07-31 15:39:08 +0530

    Data Prep Backup

--- subcoat.py
+++ subcoat.py
@@ -1,14 +1,63 @@
 subcoat = ["Unalloyed WC/Co (Tungsten Carbide with Cobalt binder), fine grained",
-           "Unalloyed WC/Co (Tungsten Carbide with Cobalt binder), coarse grained",
+           "Exceptional edge wear resistance combined with very high strength",
+           "Superior thermal deformation and depth of cut notch resistance",
+           
+           "excellent abrasion resistance",
+
            "unalloyed carbide substrate with PVD coating of TiAlN (Titanium Aluminium Nitride)",
+           "very deformation resistant",
+           "machining of hardened and short chipping materials",
+           "Roughing, semi-finishing and finishing at higher speeds",
+           
            "ultra-fine grain unalloyed substrate with PVD coating of TiAlN (Titanium Aluminium Nitride)",
+           "Tough",
+           "Roughing, semi-finishing and finishing at low to medium speeds",
+           "Handle interrupted cutting and high feed rates",
+           
            "Unalloyed carbide substrate with PVD coating of TiB2 (Titanium Diboride), fine grained",
+           "reduced surface friction, speedy chip flow and outstanding wear resistance",
+           "built-up edge is prevented because this coating has a very low affinity for aluminum",
+           "Sharp edges",
+           "Roughing, semi-finishing and finishing",
+           "excellent thermal deformation resistance and edge integrity",
+           
            "tungsten carbide substrate with 6\% cobalt and PVD coating of TiAlN (Titanium Aluminium Nitride), fine-grained",
+           "Specially designed for high-temperature alloys at speeds double the conventional PVD coating due to 6% cobalt",
+           "excellent toughness and deformation resistance",
+           
            "tungsten carbide substrate with 10\% cobalt and PVD coating of TiAlN (Titanium Aluminium Nitride), fine-grained",
+           "Higher cobalt allows for interrupted cutting",
+           "Medium and heavy interrupted cuts",
+           "Tungsten carbide resist deformation at higher speeds",
+           
            "carbide substrate and PVD coating of TiAlN (Titanium Aluminium Nitride) and TiN (Titanium Nitride), fine-grained",
+           "highly deformation resistant",
+           "Multi-layer coating",
+           "Semi finishing and finishing",
+           "machining hardened and short-chipping materials",
+           
+           "carbide substrate and PVD coating of TiAlN (Titanium Aluminium Nitride) and TiN (Titanium Nitride), fine-grained",
+           "Has higher cobalt content than KU10T grade",
+           "Suitable for grooving, threading, cut-off operations",
+           "Roughing, semi-finishing and finishing",
+           "machining hardened and short-chipping materials",
+           "Multi-layer coating",
+           
            "advanced cobalt-enriched carbide substrate and CVD coating of TiCN (Titanium Carbo Nitride), thick Al2O3(Aluminium Oxide) and TiN (Titanium Nitride), fine-grained",
+           "highly wear resistant and can operate without coolant at high speeds",
+           "Multi-layer coating",
+           "Roughing, Semi finishing and Finishing",
+           "Excellent edge strength and suitable for interrupted machining",
+           "resists workpiece build-up and microchipping",
+                      
            "cobalt-enriched Carbide substrate with CVD coating - TiN (Titanium Nitride / thick Al2O3(Aluminium Oxide) / TiC (Titanium Carbide) / TiCN (Titanium Carbo Nitride)",
+           "heavy roughing",
+           "High toughness",
+           "Multi layer coating",
+           
            "advanced cobalt-enriched carbide substrate with  CVD coating of thick MT-TiCN (medium temperature Titanium Carbo Nitride), thick Al2O3(Aluminium Oxide) and TiN (Titanium Nitride)",
+           
+           
            "Tough carbide grade Multi-layered coating TiN / Al2O3 / MT-TiCn / TiN",
            "Cobalt enriched carbide Multi-layered coating:TiN / Al2O3 / MT-TiCN / TiN",
            "Super tough carbide substrate Multilayered CVD coating:TiN / Al2O3 / MT-TiCN / TiN",
@@ -34,19 +83,50 @@ subcoat = ["Unalloyed WC/Co (Tungsten Carbide with Cobalt binder), fine grained"
 
 annotations = [ ]
 
-grades=["K313","K68","KC5010","KC5025","KC5410","KC5510","KC5525","KU10T ","KU25T ","KC8050","KC9040",
-        "KC9110","KC9125","KC9140","KC9225","KC9240","KC9245","KC9315","KC9325","KU30T ","KT315 ",
-        "KY1310","KY1525","KY1540","KY1615","KY2100","KY3400","KY3500","KY4400","KB5625","KB9610",
-        "KB9640","KD120 ","KD100 ","KD1405"]
-print(grades)
-from sklearn.feature_extraction.text import CountVectorizer
-
-vectorizer = CountVectorizer()
-vectors = vectorizer.fit_transform(train_x)
-
-print(vectorizer.get_feature_names())
-print(vectors.toarray())
-
-from sklearn import svm
-clf_svm = svm.svc(kernel='linear')
-clf_svm.fit(annotations,grades)
+grades=["K313","K313","K313",
+        "K68",
+        "KC5010","KC5010","KC5010","KC5010",
+        "KC5025","KC5025","KC5025","KC5025",
+        "KC5410","KC5410","KC5410","KC5410","KC5410","KC5410",
+        "KC5510","KC5510","KC5510",
+        "KC5525","KC5525","KC5525","KC5525",
+        "KU10T","KU10T","KU10T","KU10T","KU10T",
+        "KU25T","KU25T","KU25T","KU25T","KU25T","KU25T",
+        "KC8050","KC8050","KC8050","KC8050","KC8050","KC8050",
+        "KC9040","KC9040","KC9040","KC9040",
+        "KC9110",
+        "KC9125",
+        "KC9140",
+        "KC9225",
+        "KC9240",
+        "KC9245",
+        "KC9315",
+        "KC9325",
+        "KU30T",
+        "KT315",
+        "KY1310",
+        "KY1525",
+        "KY1540",
+        "KY1615",
+        "KY2100",
+        "KY3400",
+        "KY3500",
+        "KY4400",
+        "KB5625",
+        "KB9610",
+        "KB9640",
+        "KD120",
+        "KD100",
+        "KD1405"]
+#print(grades)
+#from sklearn.feature_extraction.text import CountVectorizer
+#
+#vectorizer = CountVectorizer()
+#vectors = vectorizer.fit_transform(train_x)
+#
+#print(vectorizer.get_feature_names())
+#print(vectors.toarray())
+#
+#from sklearn import svm
+#clf_svm = svm.svc(kernel='linear')
+#clf_svm.fit(annotations,grades)
