7f2e55522487a1431e5d48e4ec768ca8778a091b
shriramg <gshriram14@gmail.com>
2022-08-17 22:45:46 +0530

    Checkout

--- NLP/SVMInsertGeometry.py
+++ NLP/SVMInsertGeometry.py
@@ -3,17 +3,27 @@ import json
 with open('InsertGeometry.json','r') as f:
         data = json.load(f)
         
-from sklearn.feature_extraction.text import CountVectorizer
+annot = []
+for i in data:
+    for j in range(len(data[i])):
+        annot.append(data[i][j])
+print(annot)
 
-vectorizer = CountVectorizer(binary=True)
-train_annot = vectorizer.fit_transform(data['Descriptions'])
+labels = []
 
-#print(vectorizer.get_feature_names_out())
-#print(vectors.toarray())
 
-from sklearn import svm
-clf_svm = svm.SVC(kernel='linear')
-clf_svm.fit(train_annot,data['Grades'])
 
-test_x = vectorizer.transform(['I want less abrasion and less bruising'])
-print(clf_svm.predict(test_x))
+#from sklearn.feature_extraction.text import CountVectorizer
+#
+#vectorizer = CountVectorizer(binary=True)
+#train_annot = vectorizer.fit_transform(data['Descriptions'])
+#
+##print(vectorizer.get_feature_names_out())
+##print(vectors.toarray())
+#
+#from sklearn import svm
+#clf_svm = svm.SVC(kernel='linear')
+#clf_svm.fit(train_annot,data['Grades'])
+#
+#test_x = vectorizer.transform(['I want less abrasion and less bruising'])
+#print(clf_svm.predict(test_x))
