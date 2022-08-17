fcca311bf6f6ef520c8bf2f6f780feec9e3479eb
shriramg <gshriram14@gmail.com>
2022-08-17 21:41:36 +0530

    Insert Geometry JSON

--- NLP/jsosnify.py
+++ NLP/jsosnify.py
@@ -1,2 +1,15 @@
 import json 
 import pandas as pd
+
+f = pd.read_excel('Insert_Geometry.xlsx')
+NodeName = [i for i in f['property 1']]
+property = [i for i in f['property 2']]
+D = dict()
+for i in range(len(NodeName)):
+    D[NodeName[i]] = [property[i]]
+with open('Insert_Geometry.json', 'w') as f:
+    json.dump(D, f, indent = 2)
+
+
+
+
